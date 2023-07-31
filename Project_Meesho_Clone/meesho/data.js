c915155a6c02ae92674fcd99abb1e5e2dc8042c7
Tarun Kumar <94530584+tarunkumar101@users.noreply.github.com>
2023-07-31 22:55:27 +0530

    Meesho Clone

---
+++ Project_Meesho_Clone/meesho/data.js
@@ -0,0 +1,283 @@
+export default[
+    {
+        id : "product_1",
+        name : "Boat New AirPods Pro",
+        desc : "Boat New AirPods Pro with MagSafe Charging Case Visit the Apple Store",
+        price : "2999",
+        rating : "3.5",
+        review : "790",
+        category : "bluetooth Headset",
+        img : "boat_airpods.webp"
+    },
+    {
+        id : "product_2",
+        name : "Bluetooth Headset",
+        desc : "B11 Wireless Neckband Bluetooth Ear Headset Bluetooth Headset (Blue, In the Ear)",
+        price : "1200",
+        rating : "3.8",
+        review : "691",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_1.webp"
+    },
+    {
+        id : "product_3",
+        name : "Bluetooth Headset With Mic",
+        desc : "BLUETOOTH HEADSET WITH MIC MULTICOLOR PACK OF 1 PC",
+        price : "2735",
+        rating : "3.7",
+        review : "9138",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_2.webp"
+    },
+    {
+        id : "product_4",
+        name : "APPLE Airpod Pro",
+        desc : "AIRPODS PRO APPLE Airpod Pro With Wireless Charging Case Active Noise Cancelation Enabled Bluetooth Headset BLACK, True Wireless",
+        price : "19999",
+        rating : "3.9",
+        review : "2432",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_3.webp"
+    },
+    {
+        id : "product_5",
+        name : "Strip Bluetooth",
+        desc : "b11 bluetooth high bass speacial quality neckband",
+        price : "1299",
+        rating : "3.9",
+        review : "1500",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_4.webp"
+    },
+    {
+        id : "product_6",
+        name : "IPod TWS i12 Earpods",
+        desc : "BALAJI iPod TWS i12 Earpods Bluetooth Wireless Earbuds Bluetooth Headset (White, In the Ear)",
+        price : "3099",
+        rating : "3.9",
+        review : "249",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_5.webp"
+    },
+    {
+        id : "product_7",
+        name : "Boat F9-5 TWS EarPods",
+        desc : "boAt F9-5 TWS EarPods touch control Led earbuds wireless Bluetooth Headset (Black, True Wireless) Airpod",
+        price : "754",
+        rating : "3.5",
+        review : "194",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_6.webp"
+    },
+    {
+        id : "product_8",
+        name : "APPLE Airpod Pro (DUP)",
+        desc : "AIRPODS PRO APPLE Airpod Pro With Wireless Charging Case Active Noise Cancellation Enabled Bluetooth Headset , (BLACK,True Wireless)",
+        price : "2600",
+        rating : "3.9",
+        review : "189",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_7.webp"
+    },
+    {
+        id : "product_9",
+        name : "Airpod Pro with Wireless Charging",
+        desc : "Airpod Pro with Wireless Charging Case(Blue)",
+        price : "9999",
+        rating : "3.8",
+        review : "8",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_8.webp"
+    },
+    {
+        id : "product_10",
+        name : "TWS WIRELESS AIRPODS EARBUDS",
+        desc : "EARPOD EARPHONE HEADPHONE Bluetooth Earphones Touch Sensor with in Built Mic and High Bass Level Supporting",
+        price : "618",
+        rating : "3.8",
+        review : "2413",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_9.webp"
+    },
+    {
+        id : "product_11",
+        name : "B11 Neckband Sports Bluetooth",
+        desc : "B11 Neckband Sports Bluetooth Bluetooth Headset (Blue, In the Ear)",
+        price : "2109",
+        rating : "3.7",
+        review : "5443",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_10.webp"
+    },
+    {
+        id : "product_12",
+        name : "AIRPODS PRO With Active Noise Cancellation(black)",
+        desc : "AIRPODS PRO With Active Noise Cancellation(black) Bluetooth Bluetooth Headset",
+        price : "7536",
+        rating : "3.9",
+        review : "460",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_11.webp"
+    },
+    {
+        id : "product_13",
+        name : "B11 Wireless Bluetooth Headset With Mic",
+        desc : "Bluetooth Headset (Blue, In the Ear) B11 Wireless Bluetooth Headset With Mic Bluetooth Headset (Blue, In the Ear) pack of - 1",
+        price : "3249",
+        rating : "3.9",
+        review : "460",
+        category : "bluetooth Headset",
+        img : "Bluetooth_Headset_12.webp"
+    },
+    {
+        id : "product_14",
+        name : "Premium Men Gold Brass Chains",
+        desc : "Premium Men Gold Brass Chains",
+        price : "2999",
+        rating : "3.9",
+        review : "460",
+        category : "Men Chains",
+        img : "premiumGold_1.webp"
+    },
+    {
+        id : "product_15",
+        name : "Bhumi09 Golden Necklace Chain",
+        desc : "Bhumi09 Golden Necklace Chain & Bracelet For Boys , Men Stylish",
+        price : "3999",
+        rating : "3.9",
+        review : "22",
+        category : "Men Chains",
+        img : "premiumGold_2.webp"
+    },
+    {
+        id : "product_16",
+        name : "Men's Silver Chain",
+        desc : "Men's Silver Chain",
+        price : "3999",
+        rating : "3.9",
+        review : "22",
+        category : "Men Chains",
+        img : "premiumGold_3.webp"
+    },
+    {
+        id : "product_17",
+        name : "Women's Sliver Chain",
+        desc : "Stylish Women's Silver Chain",
+        price : "4999",
+        rating : "3.9",
+        review : "22",
+        category : "Men Chains",
+        img : "premiumGold_3.webp"
+    },
+    {
+        id : "product_18",
+        name : "Men's White Kurta Pyjama Set Combo",
+        desc : "Men's White Kurta Pyjama Set Combo in 100% Cotton With Multiple Colour Option And Sizes (Festival,Birthday,Wedding, Ceremony, Casual, Engagement,Party And dandiya nights)",
+        price : "599",
+        rating : "3.9",
+        review : "315",
+        category : "Kurtas",
+        img : "Kurta_1.webp"
+    },
+    {
+        id : "product_19",
+        name : "MEN'S COTTON KURTA ONLY",
+        desc : "Men's White Kurta Pyjama Set Combo in 100% Cotton With Multiple Colour Option And Sizes (Festival,Birthday,Wedding, Ceremony, Casual, Engagement,Party And dandiya nights)",
+        price : "1999",
+        rating : "3.9",
+        review : "599",
+        category : "Kurtas",
+        img : "Kurta_2.webp"
+    },
+    {
+        id : "product_20",
+        name : "Mens Kurta",
+        desc : "Mens Straigth ONLY Kurta In 100% Cotton With Multiple Colour Option And Sizes (Birthday,Wedding, Ceremony, Casual, Engagement)(only kurta)",
+        price : "1599",
+        rating : "3.9",
+        review : "599",
+        category : "Kurtas",
+        img : "Kurta_4.webp"
+    },
+    {
+        id : "product_21",
+        name : "Mobile Charger",
+        desc : "Mobile Charger",
+        price : "1450",
+        rating : "3.9",
+        review : "599",
+        category : "Mobile Accessories",
+        img : "mobile_acces_1.webp"
+    },
+    {
+        id : "product_22",
+        name : "16W Super Fast Mobile Charger",
+        desc : "16W Super Fast Mobile Charger For A9 Charger Original Adapter Universal Mobile Charger | Wall Charger Android Smartphone Travel Charger | Hi Speed Fast Dock Charger With 1 Meter Charging & Sync Micro USB/V8/LG Data Cable ( 3 Amp, 4D, White)",
+        price : "999",
+        rating : "3.9",
+        review : "599",
+        category : "Mobile Accessories",
+        img : "mobile_acces_2.webp"
+    },
+    {
+        id : "product_23",
+        name : "Vivo V 21 Mobile Back Cover, Back Cover",
+        desc : "Vivo V 21 Mobile Back Cover, Back Cover",
+        price : "899",
+        rating : "3.9",
+        review : "599",
+        category : "Mobile Accessories",
+        img : "mobile_acces_3.webp"
+    },
+    {
+        id : "product_24",
+        name : "Party Ware Saree",
+        desc : "Party Ware Saree",
+        price : "3669",
+        rating : "3.9",
+        review : "599",
+        category : "sarees",
+        img : "sarees_1.jpg"
+    },
+    {
+        id : "product_25",
+        name : "Cotton Saree",
+        desc : "Cotton Saree",
+        price : "2199",
+        rating : "3.9",
+        review : "599",
+        category : "sarees",
+        img : "sarees_2.webp"
+    },
+    {
+        id : "product_26",
+        name : "Trendy Women's Saree",
+        desc : "Trendy Women's Saree",
+        price : "2690",
+        rating : "3.9",
+        review : "599",
+        category : "sarees",
+        img : "sarees_3.webp"
+    },
+    {
+        id : "product_26",
+        name : "Women's Desinger Georgette Floral",
+        desc : "Women's Desinger Georgette Floral Printed Saree with Blouse Piece",
+        price : "2699",
+        rating : "3.9",
+        review : "599",
+        category : "sarees",
+        img : "sarees_4.webp"
+    },
+    {
+        id : "product_27",
+        name : "Boys Casual Items",
+        desc : "Boys Casual Items",
+        price : "2199",
+        rating : "3.9",
+        review : "599",
+        category : "watch",
+        img : "watch_1.webp"
+    },
+    
+]
